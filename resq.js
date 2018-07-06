1fbb6d173c69efe687162806710ebc7f42fb21db
Jack Lu <jacklu@jacklu.me>
2018-07-06 11:42:49 +0800

    +get

--- resq.js
+++ resq.js
@@ -1,3 +1,10 @@
 angular.module('resq', [])
 	.service('resq', function () {
+		return function (config) {
+			return {
+				get: function (id, type) {
+					return config.get(id, type);
+				}
+			};
+		};
 	});

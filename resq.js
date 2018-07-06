d5945762158e363a747c08d8c37268abd3215332
Jack Lu <jacklu@jacklu.me>
2018-07-06 14:38:50 +0800

    +get(Array)

--- resq.js
+++ resq.js
@@ -3,7 +3,16 @@ angular.module('resq', [])
 		return function (config) {
 			return {
 				get: function (id, type) {
-					return config.get(id, type);
+					return id instanceof Array ?
+						config.get(id, type)
+							.then(function (object) {
+								return id.map(
+									object instanceof Array ?
+										function (id, i) { return object[i]; } :
+										function (id) { return object[id]; }
+								);
+							}) :
+						config.get(id, type);
 				}
 			};
 		};

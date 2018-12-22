b749b7f5f8bad2bbc22eeec5f1abe61b9b7d22df
Jack Lu <jacklu@jacklu.me>
2018-12-22 23:41:05 +0800

    +mocha

---
+++ test/test.js
@@ -0,0 +1,50 @@
+var assert = require('assert');
+var q = require('q');
+require('any-promise/register/q');
+var Memory = require('../Memory');
+var data = require('./data');
+var r = Memory(data);
+it('should get singular', function () {
+	return r.get(1, 'post').then(post => {
+		assert.equal(post, data.post[1]);
+	});
+});
+it('should get collection', function () {
+	return r.get([1, 2], 'user').then(user => {
+		assert.equal(user[0], data.user[1]);
+		assert.equal(user[1], data.user[2]);
+	});
+});
+it('should join singular', function () {
+	return q.resolve(1).then(r.join('post')).then(post => {
+		assert.equal(post, data.post[1]);
+	});
+});
+it('should join complex', function () {
+	return q.resolve({ a: 1, b: [1, 2] })
+		.then(r.join({
+			a: ['post', {
+				userId: "user as user",
+				x: "/x",
+				y: "/y"
+			}],
+			b: [['user', {
+				x: "/x"
+			}]]
+		})).then(object => {
+			assert.equal(object.a, data.post[1]);
+			assert.equal(object.a.user, data.user[object.a.userId]);
+			assert.equal(object.a.x, `/post/1/x`);
+			assert.equal(object.a.y, `/post/1/y`);
+			assert.equal(object.b[0], data.user[1]);
+			assert.equal(object.b[0].x, `/user/1/x`);
+			assert.equal(object.b[1], data.user[2]);
+			assert.equal(object.b[1].x, `/user/2/x`);
+		});
+});
+it('should parse reference', function () {
+	return q.resolve(["post.1", "user.1"]).then(r.join([])).then(array => {
+		assert.equal(array[0], data.post[1]);
+		assert.equal(array[1], data.user[1]);
+	});
+});

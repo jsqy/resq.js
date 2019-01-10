c2f447a56d786f3eba8b62b4f122c4eeef8af765
Jack Lu <jacklu@jacklu.me>
2019-01-10 23:17:10 +0800

    Access.Memory(data, +{collection})

--- Access.Memory.js
+++ Access.Memory.js
@@ -1,11 +1,24 @@
 var q = require('q');
-module.exports = data => ({
+module.exports = (data, option = { collection: 'array' }) => (({ map }) => ({
 	get: (id, type, relation) =>
 		relation ?
 			id instanceof Array ?
-				q.resolve(id.map((v, i) => `/${type}/${v}/${relation}`)) :
+				q.resolve(map.call(id, id => `/${type}/${id}/${relation}`)) :
 				q.resolve(`/${type}/${id}/${relation}`) :
 			id instanceof Array ?
-				q.resolve(id.map(id => data[type][id])) :
+				q.resolve(map.call(id, id => data[type][id])) :
 				q.resolve(data[type][id])
+}))({
+	map:
+		option.collection == 'array' ?
+			Array.prototype.map :
+			option.collection == 'object' ?
+				mapToObject :
+				undefined
 });
+function mapToObject(f) {
+	var object = {};
+	for (var i in this)
+		object[this[i]] = f(this[i]);
+	return object;
+}

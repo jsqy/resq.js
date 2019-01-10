25f131a3241c8b8e726f6f34130cc663a915f8a6
Jack Lu <jacklu@jacklu.me>
2019-01-10 23:38:12 +0800

    Access.Memory(data, {collection, +clone})

--- Access.Memory.js
+++ Access.Memory.js
@@ -1,24 +1,32 @@
 var q = require('q');
-module.exports = (data, option = { collection: 'array' }) => (({ map }) => ({
+module.exports = (data, option = { collection: 'array', clone: false }) => (({ map, transform }) => ({
 	get: (id, type, relation) =>
 		relation ?
 			id instanceof Array ?
 				q.resolve(map.call(id, id => `/${type}/${id}/${relation}`)) :
 				q.resolve(`/${type}/${id}/${relation}`) :
 			id instanceof Array ?
-				q.resolve(map.call(id, id => data[type][id])) :
-				q.resolve(data[type][id])
+				q.resolve(map.call(id, id => transform(data[type][id]))) :
+				q.resolve(transform(data[type][id]))
 }))({
 	map:
 		option.collection == 'array' ?
 			Array.prototype.map :
 			option.collection == 'object' ?
 				mapToObject :
-				undefined
+				undefined,
+	transform:
+		option.clone ? clone : identity
 });
 function mapToObject(f) {
 	var object = {};
 	for (var i in this)
 		object[this[i]] = f(this[i]);
+	return object;
+}
+function clone(object) {
+	return JSON.parse(JSON.stringify(object));
+}
+function identity(object) {
 	return object;
 }

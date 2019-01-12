d7be81b5a0e92a434814212f41587cd72eabe16d
Jack Lu <jacklu@jacklu.me>
2019-01-12 23:13:45 +0800

    Access.Memory: relate objects

--- Access.Memory.js
+++ Access.Memory.js
@@ -1,10 +1,10 @@
 var q = require('q');
-module.exports = (data, option = {}) => (({ map, transform }) => ({
+module.exports = (data, option = {}) => (({ map, transform, relate }) => ({
 	get: (id, type, relation) =>
 		relation ?
 			id instanceof Array ?
-				q.resolve(map.call(id, id => `/${type}/${id}/${relation}`)) :
-				q.resolve(`/${type}/${id}/${relation}`) :
+				q.resolve(map.call(id, id => relate(id, type, relation))) :
+				q.resolve(relate(id, type, relation)) :
 			id instanceof Array ?
 				q.resolve(map.call(id, id => transform(data[type][id]))) :
 				q.resolve(transform(data[type][id]))
@@ -18,13 +18,28 @@ module.exports = (data, option = {}) => (({ map, transform }) => ({
 					undefined :
 			Array.prototype.map,
 	transform:
-		option.clone ? clone : identity
+		option.clone ? clone : identity,
+	relate:
+		(id, type, relation) =>
+			relation in data ?
+				filterObject.call(
+					data[relation],
+					object => object[`${type}Id`] == id
+				) :
+				`/${type}/${id}/${relation}`
 });
 function mapToObject(f) {
 	var object = {};
 	for (var i in this)
 		object[this[i]] = f(this[i]);
 	return object;
+}
+function filterObject(f) {
+	var array = [];
+	for (var i in this)
+		if (f(this[i]))
+			array.push(i);
+	return array;
 }
 function clone(object) {
 	return JSON.parse(JSON.stringify(object));
